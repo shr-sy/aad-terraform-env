15f62f1e3037ea2a7d0bf1c5a9b3c5500020235d
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-08-21 17:12:23 +0530

    Update main.tf

--- modules/users/main.tf
+++ modules/users/main.tf
@@ -1,22 +1,12 @@
-variable "users" {
-  type = list(object({
-    user_principal_name = string
-    display_name        = string
-    password            = string
-  }))
-  default = []
-}
-
 resource "azuread_user" "this" {
-  for_each = { for u in var.users : u.user_principal_name => u }
-
+  for_each             = { for u in var.users : u.user_principal_name => u }
   user_principal_name  = each.value.user_principal_name
   display_name         = each.value.display_name
-  mail_nickname        = replace(each.value.user_principal_name, "@.*$", "")
+  mail_nickname        = each.value.mail_nickname
   password             = each.value.password
-  force_password_change = true # ✅ updated attribute name
+  force_password_change = true
 }
 
 output "user_ids" {
-  value = { for upn, u in azuread_user.this : upn => u.id }
+  value = { for k, u in azuread_user.this : k => u.id }
 }
